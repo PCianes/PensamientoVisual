d7ef77f98309ca36ea32991f0e33b378a2df449e
Pablo Cianes <cianespablo@gmail.com>
2020-08-14 16:52:14 +0200

    🎨 Improve navigation and footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,10 @@ import { Link } from 'gatsby'
 const Footer = () => (
   <footer className="site-footer">
     <div className="container">
-      &copy; {new Date().getFullYear()} <Link to={`/`}>Pensamiento Visual</Link>
+      &copy; {new Date().getFullYear()}{' '}
+      <Link to={`/suscripcion`}>
+        <strong>Curso Pensamiento Visual</strong>
+      </Link>
     </div>
   </footer>
 )
